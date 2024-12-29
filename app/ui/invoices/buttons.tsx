500c9e4f4833db59ac4fb55e33b00109aa6195b9
sosojaja03 <sosojajanidze486@gmail.com>
2024-12-30 00:51:14 +0400

    change deleteInvoiceWithId

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -26,13 +26,10 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  const deleteInvoiceWithId = async (event: React.FormEvent) => {
-    event.preventDefault();
-    await deleteInvoice(id);
-  };
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
 
   return (
-    <form onSubmit={deleteInvoiceWithId}>
+    <form action={deleteInvoiceWithId}>
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-4" />

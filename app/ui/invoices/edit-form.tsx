a35ea6ab6578b675d9cd1260ea839be44e8b7292
ANITA\anita <balasanyananita@gmail.com>
2024-12-29 19:48:55 +0400

    Feat: new branch

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,8 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
+import { JSX } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -16,9 +18,10 @@ export default function EditInvoiceForm({
 }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
-}) {
+}): JSX.Element {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
-    <form>
+    <form action={async (formData) => { await updateInvoice(invoice.id, formData); }}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
